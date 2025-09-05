4249acb05a350825a148c26bfee7a5da1788a8a4
Tetiana <Korostijtatiana@gmail.com>
2025-09-05 18:26:01 +0300

    add

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -5,20 +5,48 @@ import {
 } from "@tanstack/react-query";
 import NotesClient from "./Notes.client";
 import { fetchNotes } from "@/lib/api";
-import { CATEGORIES, type Category, type CategoryNoAll } from "@/types/note";
+import { type Category, type CategoryNoAll } from "@/types/note";
 import { notFound } from "next/navigation";
+import type { Metadata } from "next";
+
+type Props = {
+  params: Promise<{ slug?: string[] }>;
+};
 
 export const dynamicParams = false;
-export function generateStaticParams() {
-  return CATEGORIES.map((c) => ({ slug: [c] }));
-}
+export const revalidate = 900;
 
-type Props = { params: Promise<{ slug?: string[] }> };
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug = [] } = await params;
+  const note = slug[0] || "All";
+
+  return {
+    title: `Notes: ${note}`,
+    description: `Notes filtered by ${note}`,
+    openGraph: {
+      title: `Notes: ${note}`,
+      description: `Notes filter: ${note}`,
+      url: `https://08-zustand-phi-three.vercel.app//notes/filter/${note}`,
+      siteName: "NoteHub",
+      images: [
+        {
+          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+          width: 1200,
+          height: 630,
+          alt: note,
+        },
+      ],
+      type: "article",
+    },
+  };
+}
 
 export default async function Page({ params }: Props) {
   const { slug = [] } = await params;
   const tag = slug[0] as Category | undefined;
-  if (!tag || !CATEGORIES.includes(tag)) notFound();
+
+  // якщо немає slug взагалі → 404
+  if (!tag) notFound();
 
   const category: CategoryNoAll | undefined =
     tag === "All" ? undefined : (tag as CategoryNoAll);
